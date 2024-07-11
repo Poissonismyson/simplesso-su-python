da557edcc8df3934e260d66ebf16ab903861e571
mrc <md.dovidio@gmail.com>
2024-07-11 15:01:37 +0200

    pulizia

--- simplesso.py
+++ simplesso.py
@@ -1,13 +1,10 @@
-def findbase(m, tipo = 'b'): # inserisci la matrice e restituisce il vettore XB0, altrimenti X_N se inserisci 'c'
-    B = [0,0,0]
+def find_base(m, tipo='b'): # tipo = 'b' per base, 'c' per non base
+    B = [0, 0, 0]
     X_N = []
 
-
     for j in range(len(m[0])):
-    
-        v_colonna = [m[0][j],m[1][j],m[2][j]]
+        v_colonna = [m[0][j], m[1][j], m[2][j]]
         
-    
         if v_colonna == [1, 0, 0]:
             B[0] = j
         elif v_colonna == [0, 1, 0]:
@@ -22,72 +19,57 @@ def findbase(m, tipo = 'b'): # inserisci la matrice e restituisce il vettore XB0
     else:
         return B
     
-def calcolaGammaZero(N_T, c_B, c_N):
+def calcola_gamma_zero(N_T, c_B, c_N): # N_T = matrice non di base, c_B = vettore variabili di base della funzione obiettivo, c_N = vettore variabili non di base della funzione obiettivo
     vet = []
     for i in range(len(N_T)):
         sum = 0
         for j in range(len(N_T[i])):
             sum += c_B[j] * N_T[i][j] 
-        vet.append(c_N[i]- sum) 
+        vet.append(c_N[i] - sum) 
     return vet
 
-def calcolaMatriceNonDiBase(m, x_N):
+def calcola_matrice_non_di_base(m, x_N): # m = matrice, x_N = vettore delle variabili non di base
     N_T = []
-    for n in x_N: # funzione per ottenere matrice non di base
+    for n in x_N:
         v = []
         for i in range(len(m)):
             v.append(m[i][n])
         N_T.append(v)
     return N_T
 
-def testIllimitatezza(g_0, N_T):
+def test_illimitatezza(g_0, N_T): # g_0 = vettore dei costi ridotti, N_T = matrice non di base
     test_tot = False
     for k in range(len(g_0)): 
-      if g_0[k] < 0: # stiamo prendendo le componenti negative di gamma
+        if g_0[k] < 0:
             col_neg = True
             for i in range(len(N_T[k])): 
-              if N_T[k][i] > 0:
-                  col_neg = False
+                if N_T[k][i] > 0:
+                    col_neg = False
             if col_neg == True:
                 test_tot = True
 
     return test_tot
 
+u = [1, 2, 1, 1, 1, 1]
+m = [[1, 2, 3, 1, 0, 0], [2, -1, -5, 0, 1, 0], [1, 2, -1, 0, 0, 1]]
+B = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
+x_b = find_base(m, 'b') #vettore degli indici delle variabili di base
+x_n = find_base(m, 'c') #vettore degli indici delle variabili non di base
+n_t = calcola_matrice_non_di_base(m, x_b) #matrice non di base
+c_b = [u[k] for k in x_b] #vettore delle variabili di base della funzione obiettivo 
+c_n = [u[k] for k in x_b] #vettore delle variabili non di base della funzione obiettivo
 
-u= [1, 2, 1, 1, 1, 1] # vettore direzione di min o max
+print(f"x_b: {x_b}")
+print(f"x_n: {x_n}")
+print(f"c_b: {c_b}")
+print(f"c_n: {c_n}")
+print(f"n_t: {n_t}")
 
-m =  [[1, 2, 3, 1, 0, 0], [2, -1, -5, 0, 1, 0], [1, 2, -1, 0, 0, 1]] # matrice di partenza
-B = [[1, 0, 0],[0, 1, 0],[0, 0, 1]] #base canonica
-
-x_B = findbase(m) # vettore indici di base
-
-x_N = findbase(m, 'c') # vettore indici non di base
-
-N_T = calcolaMatriceNonDiBase(m, x_N) # matrice non di base trasposta
-
-
-
-c_B = [u[k] for k in x_B] #vettore c_b
-
-c_N = [u[k] for k in x_N] #vettore c_n
-
-print(f"x_B: {x_B}")
-print(f"x_N: {x_N}")
-print(f"c_B: {c_B}")
-print(f"c_N: {c_N}")
-print(f"N_T: {N_T}")
-
-
-
-g_0 = calcolaGammaZero(N_T, c_B, c_N) #questa è la nostra gamma_0
-
+g_0 = calcola_gamma_zero(n_t, c_b, c_n)
 print(f"Gamma zero: {g_0}")
 
-print("test di illimitatezza: ", end='')
+print("test di illimitatezza: ", end='') 
+print(test_illimitatezza(g_0, n_t)) #test di illimitatezza
 
-print(testIllimitatezza(g_0, N_T))
-
-
-
-
+variabile_entrante = 0
